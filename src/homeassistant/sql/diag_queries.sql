41407992dd3a232348445179c8c4081ffec31093
Szabolcs Hodossy <szabolcs.hodossy-ext@man-es.com>
2025-10-11 06:32:12 +0200

    feat(homeassistant): add new diag queries
    Signed-off-by: Szabolcs Hodossy <szabolcs.hodossy-ext@man-es.com>

--- src/homeassistant/sql/diag_queries.sql
+++ src/homeassistant/sql/diag_queries.sql
@@ -27,3 +27,36 @@ select
 from information_schema.tables
 where table_schema = 'public'
 order by 3 desc;
+
+SELECT
+  date_trunc('day', to_timestamp(last_updated_ts)),
+  count(*)
+FROM states
+GROUP BY 1
+ORDER BY 1 DESC;
+
+-- find what eats the space of a table: replace public.tbl with the table name
+-- https://stackoverflow.com/questions/62642234/index-size-after-autovacuum
+SELECT l.metric, l.nr AS bytes
+     , CASE WHEN is_size THEN pg_size_pretty(nr) END AS bytes_pretty
+     , CASE WHEN is_size THEN nr / NULLIF(x.ct, 0) END AS bytes_per_row
+FROM  (
+   SELECT min(tableoid)        AS tbl      -- = 'public.tbl'::regclass::oid
+        , count(*)             AS ct
+        , sum(length(t::text)) AS txt_len  -- length in characters
+   FROM   public.tbl t                     -- provide table name *once*
+   ) x
+CROSS  JOIN LATERAL (
+   VALUES
+     (true , 'core_relation_size'               , pg_relation_size(tbl))
+   , (true , 'visibility_map'                   , pg_relation_size(tbl, 'vm'))
+   , (true , 'free_space_map'                   , pg_relation_size(tbl, 'fsm'))
+   , (true , 'table_size_incl_toast'            , pg_table_size(tbl))
+   , (true , 'indexes_size'                     , pg_indexes_size(tbl))
+   , (true , 'total_size_incl_toast_and_indexes', pg_total_relation_size(tbl))
+   , (true , 'live_rows_in_text_representation' , txt_len)
+   , (false, '------------------------------'   , NULL)
+   , (false, 'row_count'                        , ct)
+   , (false, 'live_tuples'                      , pg_stat_get_live_tuples(tbl))
+   , (false, 'dead_tuples'                      , pg_stat_get_dead_tuples(tbl))
+   ) l(is_size, metric, nr);
